e37c128be7c38af60764bafcd12ed8741772c029
Rahmat Hidayat <yayatooo@MacBook-Air.local>
2026-08-29 21:56:42 +0700

    setup index db for compatiblity in neon database

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,5 +1,3 @@
-import "dotenv/config";
-
 import { drizzle } from "drizzle-orm/node-postgres";
 
 import * as schema from "./schema";
@@ -7,9 +5,15 @@ import * as schema from "./schema";
 const databaseUrl = process.env.DATABASE_URL;
 
 if (!databaseUrl) {
-  throw new Error("DATABASE_URL is not defined");
+	throw new Error("DATABASE_URL is not defined");
 }
 
-export const db = drizzle(databaseUrl, {
-  schema,
+export const db = drizzle({
+	connection: {
+		connectionString: databaseUrl,
+		max: 1,
+		maxUses: 1,
+		idleTimeoutMillis: 5_000,
+	},
+	schema,
 });
